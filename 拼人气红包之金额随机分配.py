512ad4e24a5467882eaaf62bc684676ddc960437
Lei Shen <sctdc1@126.com>
2016-02-12 16:38:56 +0800

    Update 拼人气红包之金额随机分配.py

--- 拼人气红包之金额随机分配.py
+++ 拼人气红包之金额随机分配.py
@@ -2,21 +2,26 @@ import random
 
 
 def allocate_money(total, num):
+    # total-总金额，num-分配份数
     total = round(total, 2)
     num = int(num)
     if total <= 0 or num <= 0 or total > 200 or num > 200:
         return
 
+    # 初始化列表，为每一个列表元素分配一个随机量，并计算随机量总和 
     result = []
     radnum_sum = 0
     for i in range(num):
         result.append(random.random())
         radnum_sum += result[i]
 
+    # 总金额除以随机量总和，得到一个计算基数
+    # 计算基数乘以每一个列表元素随机量，得到一份随机分配金额 
     total_base = total/radnum_sum
     for i in range(num):
         result[i] = round(total_base*result[i], 2)
 
+    # 将累计误差加至某一列表元素，判断结果是否为零及负数，选择相加或下一个元素 
     check_sum = round(total-sum(result), 2)
     if check_sum != 0:
         for i in range(num):
