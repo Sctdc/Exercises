5c6df80d790621b9e198b0ea4582c705ac5023b2
Lei Shen <sctdc1@126.com>
2016-02-15 05:17:39 +0800

    Update 拼人气红包之金额随机分配.py

--- 拼人气红包之金额随机分配.py
+++ 拼人气红包之金额随机分配.py
@@ -2,34 +2,33 @@ import random
 
 
 def allocate_money(total, num):
-    # 初始化总金额(total，保留2位小数），分配份数（num，取整）
     total = round(total, 2)
     num = int(num)
     if total <= 0 or num <= 0 or total > 200 or num > 200:
         return
 
-    # 初始化列表，为每一个列表元素分配一个随机量
-    # 总金额除以列表元素随机量总和，得到计算基数
-    # 计算基数乘以每一个列表元素随机量，得到一份随机分配金额 
     result = [random.random() for no in range(num)]
     total_base = total/sum(result)
     for no in range(num):
         result[no] = round(total_base*result[no], 2)
 
-    # 将累计误差加至某一列表元素，若结果大于零，退出，否则选择下一列表元素
-    check_sum = round(total-sum(result), 2)
+    check_sum = total-sum(result)
     if check_sum != 0:
         for no in range(num):
             if (result[no]+check_sum) > 0:
-                result[no] += check_sum
+                result[no] = round(result[no]+check_sum, 2)
                 break
 
     return result
 
 if __name__ == "__main__":
-    result = allocate_money(99, 9)
-    for item in result:
-        print(item)
-    # 验证总金额 
-    print('Sum:', sum(result))
-    print('Err:', 99-sum(result), round(99-sum(result), 2))
+    total = 18
+    num = 6
+    result = allocate_money(total, num)
+    if result is not None:
+        for item in result:
+            print(item)
+        print('Sum:', sum(result))
+        print('Err:', total-sum(result), round(total-sum(result), 2))
+    else:
+        print('List is None.')
