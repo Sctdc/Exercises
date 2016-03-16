bd36b4b44b0413d0db924ad2437c9e85ca49ddf5
Lei Shen <sctdc1@126.com>
2016-03-16 21:21:47 +0800

    Update 必应背景图下载器.cs

--- 必应背景图下载器.cs
+++ 必应背景图下载器.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
@@ -6,28 +6,28 @@ using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace BingWallpaper
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string picDate = DateTime.Now.Year.ToString() + "_" + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Day.ToString();
-            // 获取背景图数据
-            string requestUri = @"http://www.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1&video=1";
-            WebRequest request = WebRequest.Create(requestUri);
-            WebResponse response = request.GetResponse();
-            Stream respStream = response.GetResponseStream();
-            StreamReader streamReader = new StreamReader(respStream, Encoding.GetEncoding("utf-8"));
-            // 解析背景图下载地址
-            var dicts = JsonConvert.DeserializeObject<Dictionary<string, object>>(streamReader.ReadLine());
-            var images = (JArray)dicts["images"];
-            requestUri = images[0]["url"].ToString();
-            // 保存背景图
-            WebClient client = new WebClient();
-            client.DownloadFile(requestUri, "./Wallpaper/" + picDate + ".jpg");
+        // 文件名格式为'年_月_日.jpg'
+        string picDate = DateTime.Now.Year.ToString() + "_"
+                       + DateTime.Now.Month.ToString() + "_"
+                       + DateTime.Now.Day.ToString();
+        // 获取背景图数据
+        string requestUri = @"http://www.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1&video=1";
+        var request = WebRequest.Create(requestUri);
+        var response = request.GetResponse();
+        var respStream = response.GetResponseStream();
+        var streamReader = new StreamReader(respStream, Encoding.GetEncoding("utf-8"));
+        // 解析背景图下载地址
+        var dicts = JsonConvert.DeserializeObject<Dictionary<string, object>>(streamReader.ReadLine());
+        var images = (JArray)dicts["images"];
+        requestUri = images[0]["url"].ToString();
+        // 保存背景图，下载目录为当前目录下/Wallpaper子目录
+        var client = new WebClient();
+        client.DownloadFile(requestUri, "./Wallpaper/" + picDate + ".jpg");
 
-            Console.WriteLine("今日必应背景图下载完毕！");
-        }
+        Console.WriteLine("今日必应背景图下载完毕！");
     }
 }
