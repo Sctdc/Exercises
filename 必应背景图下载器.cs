dcdc7c173e3da57145a2d1d6109b783c1fdb6bb5
Lei Shen <sctdc1@126.com>
2016-03-19 15:27:58 +0800

    Update 必应背景图下载器.cs

--- 必应背景图下载器.cs
+++ 必应背景图下载器.cs
@@ -10,24 +10,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        // 文件名格式为'年_月_日.jpg'
-        string picDate = DateTime.Now.Year.ToString() + "_"
-                       + DateTime.Now.Month.ToString() + "_"
-                       + DateTime.Now.Day.ToString();
-        // 获取背景图数据
         string requestUri = @"http://www.bing.com/HPImageArchive.aspx?format=js&mbl=1&idx=0&n=1&video=1";
-        var request = WebRequest.Create(requestUri);
-        var response = request.GetResponse();
-        var respStream = response.GetResponseStream();
-        var streamReader = new StreamReader(respStream, Encoding.GetEncoding("utf-8"));
-        // 解析背景图下载地址
-        var dicts = JsonConvert.DeserializeObject<Dictionary<string, object>>(streamReader.ReadLine());
-        var images = (JArray)dicts["images"];
-        requestUri = images[0]["url"].ToString();
-        // 保存背景图，下载目录为当前目录下/Wallpaper子目录
-        var client = new WebClient();
-        client.DownloadFile(requestUri, "./Wallpaper/" + picDate + ".jpg");
+        string picDate;
+        var dicts = new Dictionary<string, object>();
+
+        try
+        {
+            var request = WebRequest.Create(requestUri);
+            var response = request.GetResponse();
+            var respStream = response.GetResponseStream();
+            var streamReader = new StreamReader(respStream, Encoding.GetEncoding("utf-8"));
+            dicts = JsonConvert.DeserializeObject<Dictionary<string, object>>(streamReader.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("获取背景图信息出错！按任意键退出......");
+            Console.ReadKey();
+            goto Exit;
+        }
+
+        try
+        {
+            var images = (JArray)dicts["images"];
+            picDate = images[0]["startdate"].ToString();
+            requestUri = images[0]["url"].ToString();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("解析背景图信息出错！按任意键退出......");
+            Console.ReadKey();
+            goto Exit;
+        }
+
+        try
+        {
+            var client = new WebClient();
+            client.DownloadFile(requestUri, "./Wallpaper/" + picDate + ".jpg");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("保存背景图出错！按任意键退出......");
+            Console.ReadKey();
+            goto Exit;
+        }
 
         Console.WriteLine("今日必应背景图下载完毕！");
+
+    Exit:;
     }
 }
